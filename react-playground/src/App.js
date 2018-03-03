77a22232bdb06ee9d2191e8022056ea34f55be93
Wolfger Schramm <wolfger@spearwolf.de>
2018-03-03 14:19:44 +0100

    fix eslint setup

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Picimo from './Picimo';
 
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
 
-        <Picimo.Canvas />
+    <Picimo.Canvas />
 
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload!
-        </p>
-      </div>
-    );
-  }
-}
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload!
+    </p>
+  </div>
+);
 
 export default App;
