71f74200c87576e84e2217bc936a62a839e45c41
Wolfger Schramm <wolfger@spearwolf.de>
2018-03-03 15:26:15 +0100

    add onFrame to <Picimo.Canvas />

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -12,7 +12,7 @@ const App = () => (
       <h1 className="App-title">Welcome to React</h1>
     </header>
 
-    <Picimo.Canvas />
+    <Picimo.Canvas alpha />
 
     <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload!
--- react-playground/src/Picimo/Canvas.js
+++ react-playground/src/Picimo/Canvas.js
@@ -1,28 +1,97 @@
+/* eslint-env browser */
+/* eslint no-param-reassign: 0 */
+/* eslint react/no-unused-prop-types: 0 */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import pick from 'lodash';
 
-import { WebGlRenderer } from '@picimo/renderer';
+import { WebGlRenderer } from '@picimo/renderer'; // eslint-disable-line
 
 const initRenderer = (component, canvas) => {
-  component.canvas = canvas;
-  component.renderer = new WebGlRenderer(canvas);
+  if (!canvas) return;
+  if (component.renderer) return;
+  if (component.canvas && canvas !== component.canvas) {
+    throw new Error('initRenderer: canvas element changed!');
+  }
 
-  console.log('initRenderer:', component, component.canvas, component.renderer);
+  component.canvas = canvas;
+
+  const options = pick(component.props, [
+    'alpha',
+    'depth',
+    'stencil',
+    'antialias',
+    'premultipliedAlpha',
+    'preserveDrawingBuffer',
+    'pixelRatio',
+  ]);
+
+  component.renderer = new WebGlRenderer(canvas, options);
+
+  console.log('initRenderer:', component, component.canvas, component.renderer); // eslint-disable-line
 };
 
 class Canvas extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-  };
+  constructor(props) {
+    super(props);
+    this.rafId = 0;
+  }
+
+  componentDidMount() {
+    const animate = () => {
+      this.rafId = window.requestAnimationFrame((now) => {
+        animate();
+        this.onFrame(now);
+      });
+    };
+    animate();
+  }
+
+  componentWillUnmount() {
+    window.cancelAnimationFrame(this.rafId);
+  }
+
+  onFrame(now) {
+    this.renderer.render(now);
+    this.forceUpdate();
+  }
 
   render() {
     return (
-      <div style={{ width: '320px', height: '200px', display: 'inline-block', margin: '0 auto' }}>
+      <div style={{
+          width: '320px',
+          height: '200px',
+          display: 'inline-block',
+          margin: '0 auto',
+        }}
+      >
         <canvas ref={canvas => initRenderer(this, canvas)} />
         { this.props.children }
       </div>
     );
   }
 }
+
+Canvas.propTypes = {
+  children: PropTypes.node,
+  alpha: PropTypes.bool,
+  depth: PropTypes.bool,
+  stencil: PropTypes.bool,
+  antialias: PropTypes.bool,
+  premultipliedAlpha: PropTypes.bool,
+  preserveDrawingBuffer: PropTypes.bool,
+  pixelRatio: PropTypes.number,
+};
+
+Canvas.defaultProps = {
+  children: undefined,
+  alpha: false,
+  depth: true,
+  stencil: true,
+  antialias: false,
+  premultipliedAlpha: true,
+  preserveDrawingBuffer: false,
+  pixelRatio: undefined,
+};
 
 export default Canvas;
