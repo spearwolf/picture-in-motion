c1a3b9460c3f000c58dfa5448495bd4ecbdac95a
Wolfger Schramm <wolfger@spearwolf.de>
2018-05-02 14:19:02 +0200

    improve AppShell hilite style

--- examples/src/AppShell/AppShell.js
+++ examples/src/AppShell/AppShell.js
@@ -88,10 +88,10 @@ const DemoLink = styled.a`
   font-size: 14px;
   text-decoration: none;
   color: ${props => (props.active ? '#FF3060' : '#28C')};
-  background-color: ${props => (props.active ? '#F2F2F2' : 'transparent')};
-  padding: 5px 14px 3px;
+  background-color: ${props => (props.active ? '#ffe8e7' : 'transparent')};
+  padding: 5px 14px 3px 35px;
   border-radius: 16px;
-  margin-left: -13px;
+  margin-left: -33px;
   cursor: pointer;
 
   &:hover {
