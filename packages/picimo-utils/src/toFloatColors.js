6517d7606f95d0b6b2df1e1623dc7c9120aab814
Wolfger Schramm <wolfger@spearwolf.de>
2018-10-19 09:47:10 +0200

    improve jsdoc comments

--- packages/picimo-utils/src/toFloatColors.js
+++ packages/picimo-utils/src/toFloatColors.js
@@ -1,5 +1,6 @@
 
 /**
- * Converts an array containing (3-4)x color values [0..255] to float values [0..1]
+ * Converts an array containing (3-4)x color values [0..255] to float values [0..1].
+ * Returns a new array, the old one will be untouched.
  */
 export default colors => colors.map(col => col / 255);
