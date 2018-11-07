6948321804a481941f591d4514f74a2af64c7ca1
Wolfger Schramm <wolfger@spearwolf.de>
2018-11-07 08:31:03 +0100

    add karma istanbul coverage report to @picimo/utils

--- packages/picimo-utils/karma.conf.js
+++ packages/picimo-utils/karma.conf.js
@@ -1,4 +1,5 @@
 // Karma configuration
+const path = require('path');
 
 module.exports = (config) => {
   config.set({
@@ -29,6 +30,7 @@ module.exports = (config) => {
       '**/*.spec.js': ['webpack', 'sourcemap'],
     },
 
+
     webpack: {
       mode: 'development',
       devtool: 'inline-source-map',
@@ -41,6 +43,11 @@ module.exports = (config) => {
             use: {
               loader: 'babel-loader',
               options: {
+                plugins: [
+                  ['istanbul', {
+                    exclude: ['**/*.spec.js'],
+                  }],
+                ],
                 presets: [
                   ['@babel/preset-env', {
                     debug: false,
@@ -68,8 +75,66 @@ module.exports = (config) => {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: [
+      'mocha',
+      'coverage-istanbul',
+    ],
 
+
+    // any of these options are valid: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
+    coverageIstanbulReporter: {
+      // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
+      reports: ['html', 'lcovonly', 'text-summary'],
+
+      // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
+      dir: path.join(__dirname, 'coverage'),
+
+      // Combines coverage information from multiple browsers into one report rather than outputting a report
+      // for each browser.
+      combineBrowserReports: true,
+
+      // if using webpack and pre-loaders, work around webpack breaking the source path
+      fixWebpackSourcePaths: true,
+
+      // Omit files with no statements, no functions and no branches from the report
+      skipFilesWithNoCoverage: true,
+
+      // Most reporters accept additional config options. You can pass these through the `report-config` option
+      'report-config': {
+        // all options available at: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib/html/index.js#L135-L137
+        html: {
+          // outputs the report in ./coverage/html
+          subdir: 'html',
+        },
+      },
+
+      // enforce percentage thresholds
+      // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
+      thresholds: {
+        emitWarning: true, // set to `true` to not fail the test command when thresholds are not met
+        // thresholds for all files
+        global: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100,
+        },
+        // thresholds per file
+        // each: {
+        // statements: 100,
+        // lines: 100,
+        // branches: 100,
+        // functions: 100,
+        // overrides: {
+        // 'baz/component/**/*.js': {
+        // statements: 98
+        // }
+        // }
+        // }
+      },
+
+      verbose: false, // output config used by istanbul for debugging
+    },
 
     // web server port
     port: 9876,
