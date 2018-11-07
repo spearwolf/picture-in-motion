bd6c6380c34638a3332706b2e7ef2fc97156761a
Wolfger Schramm <wolfger@spearwolf.de>
2018-11-07 08:56:48 +0100

    add karma assets proxy conf; setup karma tests for @picimo/core

---
+++ shared/karma.conf.js
@@ -0,0 +1,180 @@
+// Karma configuration
+const path = require('path');
+
+module.exports = basePath => (config) => {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath,
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'src/**/*.spec.js',
+      {
+        pattern: 'test/assets/**/*', watched: true, included: false, served: true, nocache: false,
+      },
+    ],
+
+    proxies: {
+      '/assets/': '/base/test/assets/',
+    },
+
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      '**/*.spec.js': ['webpack', 'sourcemap'],
+    },
+
+
+    webpack: {
+      mode: 'development',
+      devtool: 'inline-source-map',
+
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                plugins: [
+                  ['istanbul', {
+                    exclude: ['**/*.spec.js'],
+                  }],
+                ],
+                presets: [
+                  ['@babel/preset-env', {
+                    debug: false,
+                    useBuiltIns: 'usage',
+                    targets: {
+                      browsers: [
+                        'chrome 70',
+                      ],
+                    },
+                  }],
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+
+    webpackMiddleware: {
+      logLevel: 'error',
+      stats: 'errors-only',
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: [
+      'mocha',
+      'coverage-istanbul',
+    ],
+
+
+    // any of these options are valid: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
+    coverageIstanbulReporter: {
+      // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
+      reports: ['html', 'lcovonly', 'text-summary'],
+
+      // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
+      dir: path.join(basePath, 'coverage'),
+
+      // Combines coverage information from multiple browsers into one report rather than outputting a report
+      // for each browser.
+      combineBrowserReports: true,
+
+      // if using webpack and pre-loaders, work around webpack breaking the source path
+      fixWebpackSourcePaths: true,
+
+      // Omit files with no statements, no functions and no branches from the report
+      skipFilesWithNoCoverage: true,
+
+      // Most reporters accept additional config options. You can pass these through the `report-config` option
+      'report-config': {
+        // all options available at: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib/html/index.js#L135-L137
+        html: {
+          // outputs the report in ./coverage/html
+          subdir: 'html',
+        },
+      },
+
+      // enforce percentage thresholds
+      // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
+      thresholds: {
+        emitWarning: true, // set to `true` to not fail the test command when thresholds are not met
+        // thresholds for all files
+        global: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100,
+        },
+        // thresholds per file
+        // each: {
+        // statements: 100,
+        // lines: 100,
+        // branches: 100,
+        // functions: 100,
+        // overrides: {
+        // 'baz/component/**/*.js': {
+        // statements: 98
+        // }
+        // }
+        // }
+      },
+
+      verbose: false, // output config used by istanbul for debugging
+    },
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [
+      'Chrome',
+      'Firefox',
+    ],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+  });
+};
